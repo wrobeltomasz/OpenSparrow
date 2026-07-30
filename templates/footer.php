a2b28e6a2fb243af2e1d7053807d54582c54254e
Tomasz Wróbel <tomaszwrobel@op.pl>
2026-07-30 18:07:17 +0200

    Add license exception and update footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,11 +1,10 @@
 <footer>
     <div class="footer-content">
-        <a href="https://opensparrow.org/" class="footer-logo-link">
-            <img src="assets/img/logo-brown.png" alt="Sparrow Logo" class="footer-logo-img" />
-        </a>
         <div class="footer-text">
-            <a href="https://opensparrow.org/">OpenSparrow.org</a>
-            <span>Open source | LGPL v3. | PHP + vanilla JS + Postgres</span>
+            <span>Copyright © 2026 <a href="https://opensparrow.org/">OpenSparrow</a> Team</span>
+			<span>
+			Licensed under LGPL-3.0
+			</span>
         </div>
     </div>
 </footer>
